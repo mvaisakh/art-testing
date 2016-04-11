b138184752e17953bc0d166063718a9e12b4b5ac
Alexandre Rames <alexandre.rames@linaro.org>
2016-04-11 13:19:19 +0100

    Update `PrintDiff` to not print anything for inexistent data.
    Previously we would 'fill the blanks' with default data (`0.0`).
    
    Sample before:
                                                                       mean1  stdev1 (% of mean1)   mean2  stdev2 (% of mean2)  (mean2 - mean1) / mean1 * 100
    -----------------------------------------------------------  -----------  -------------------  ------  -------------------  -----------------------------
    benchmarks/micro/Base64.Decode                                180702.890                1.012   0.000                  inf                       -100.000
    benchmarks/micro/Base64.Encode                               1028651.594                1.619   0.000                  inf                       -100.000
    benchmarks/micro/ShifterOperand.IntSingleUseLatency1               0.000                  inf   7.292                1.017                            inf
    benchmarks/micro/ShifterOperand.IntSingleUseLatency1Thread2        0.000                  inf  11.292                0.213                            inf
    
    Sample after:
                                                                       mean1  stdev1 (% of mean1)   mean2  stdev2 (% of mean2)  (mean2 - mean1) / mean1 * 100
    -----------------------------------------------------------  -----------  -------------------  ------  -------------------  -----------------------------
    benchmarks/micro/Base64.Decode                                180702.890                1.012
    benchmarks/micro/Base64.Encode                               1028651.594                1.619
    benchmarks/micro/ShifterOperand.IntSingleUseLatency1                                            7.292                1.017
    benchmarks/micro/ShifterOperand.IntSingleUseLatency1Thread2                                    11.292                0.213
    
    Change-Id: Ic67be0f6808e5d591ba481ddc82281b7fd88dac6

--- compare.py
+++ compare.py
@@ -32,37 +32,47 @@ def BuildOptions():
     utils.AddCommonCompareOptions(parser)
     return parser.parse_args()
 
+def IsDictionaryOrNone(d):
+    return isinstance(d, OrderedDict) or isinstance(d, dict) or d is None
+
+def IsListOrNone(d):
+    return isinstance(d, list) or d is None
+
 def PrintDiff(data_1, data_2, key=None, indentation=''):
     indentation_level = '    '
-    if data_1 or data_2:
-        if (isinstance(data_1, OrderedDict) or isinstance(data_1, dict) or data_1 is None) and \
-           (isinstance(data_2, OrderedDict) or isinstance(data_2, dict) or data_2 is None):
-            if key is not None:
-                print(indentation + key)
-            entries = []
-            list_1 = list(data_1.keys()) if data_1 else []
-            list_2 = list(data_2.keys()) if data_2 else []
-            for k in utils.MergeLists(list_1, list_2):
-                value_1 = data_1[k] if data_1 and k in data_1 else None
-                value_2 = data_2[k] if data_2 and k in data_2 else None
-                maybe_entry = PrintDiff(value_1, value_2, k, indentation + indentation_level)
-                if maybe_entry is not None:
-                    entries.append(maybe_entry)
-            if entries:
-                utils_print.PrintTable([''] + utils_stats.stats_diff_headers,
-                                       entries,
-                                       line_start=indentation)
-                print('')
-        elif (isinstance(data_1, list) or data_1 is None) and \
-             (isinstance(data_2, list) or data_2 is None):
-            list_1 = data_1 if data_1 else [0.0]
-            list_2 = data_2 if data_2 else [0.0]
-            m1, M1, ave1, d1, dp1 = utils_stats.ComputeStats(list_1)
-            m2, M2, ave2, d2, dp2 = utils_stats.ComputeStats(list_2)
-            return [key] + \
-                   [ave1, dp1, ave2, dp2, utils_stats.GetRelativeDiff(ave1, ave2)]
-        elif type(data_1) != type(data_2):
-            utils.Error('The data types differ between result sets.')
+
+    if not data_1 and not data_2:
+        # There is nothing to compare or print.
+        return
+
+    if IsDictionaryOrNone(data_1) and IsDictionaryOrNone(data_2):
+        if key is not None:
+            print(indentation + key)
+        entries = []
+        list_1 = list(data_1.keys()) if data_1 else []
+        list_2 = list(data_2.keys()) if data_2 else []
+        for k in utils.MergeLists(list_1, list_2):
+            value_1 = data_1[k] if data_1 and k in data_1 else None
+            value_2 = data_2[k] if data_2 and k in data_2 else None
+            maybe_entry = PrintDiff(value_1, value_2, k, indentation + indentation_level)
+            if maybe_entry is not None:
+                entries.append(maybe_entry)
+        if entries:
+            utils_print.PrintTable([''] + utils_stats.stats_diff_headers,
+                                   entries,
+                                   line_start=indentation)
+            print('')
+    elif IsListOrNone(data_1) and IsListOrNone(data_2):
+        no_results = ('', '', '', '', '')
+        _, _, ave1, _, dp1 = \
+                utils_stats.ComputeStats(data_1) if data_1 else no_results
+        _, _, ave2, _, dp2 = \
+                utils_stats.ComputeStats(data_2) if data_2 else no_results
+        diff = utils_stats.GetRelativeDiff(ave1, ave2) \
+                if data_1 and data_2 else ''
+        return [key, ave1, dp1, ave2, dp2, diff]
+    elif type(data_1) != type(data_2):
+        utils.Error('The data types differ between result sets.')
 
 if __name__ == "__main__":
     args = BuildOptions()
