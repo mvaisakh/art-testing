54a3c41f63775db550d0d068e71c55e055426349
Alexandre Rames <alexandre.rames@linaro.org>
2016-04-28 11:26:18 +0100

    Use the result of computations in the ceil/floor benchmark.
    This helps ensure that the code is not removed by the dead code
    elimination optimization.
    
    Change-Id: Ic9b5dd13a57e9bf794344eced2f09118f09e6f81

--- benchmarks/micro/intrinsics/Rounding.java
+++ benchmarks/micro/intrinsics/Rounding.java
@@ -38,6 +38,12 @@ public class Rounding {
   private static final float[] rand_float = new float[NUM_RANDS];
   private static final double[] rand_double = new double[NUM_RANDS];
 
+  // These are written but not read. The computation routines below store their
+  // result to these static variables to ensure the computation code is not
+  // removed by DCE.
+  private static float res_float;
+  private static double res_double;
+
   static {
     Random random = new Random(42L);
     for (int i = 0; i < NUM_RANDS; i++) {
@@ -53,6 +59,7 @@ public class Rounding {
         res += Math.floor(rand_float[i % NUM_RANDS]);
       }
     }
+    res_float = res;
   }
 
   public void timeFloorDouble(int iterations) {
@@ -62,6 +69,7 @@ public class Rounding {
         res += Math.floor(rand_double[i % NUM_RANDS]);
       }
     }
+    res_double = res;
   }
 
   public void timeCeilFloat(int iterations) {
@@ -71,6 +79,7 @@ public class Rounding {
         res += Math.ceil(rand_float[i % NUM_RANDS]);
       }
     }
+    res_float = res;
   }
 
   public void timeCeilDouble(int iterations) {
@@ -80,6 +89,7 @@ public class Rounding {
         res += Math.ceil(rand_double[i % NUM_RANDS]);
       }
     }
+    res_double = res;
   }
 
   public static void main(String[] args) {
