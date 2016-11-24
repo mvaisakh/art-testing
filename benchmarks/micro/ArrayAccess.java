cb667be083ef0eb4615baab568ac9a463a4fe9e6
Michele Di Giorgio <michele.digiorgio@linaro.org>
2016-11-24 17:47:13 +0000

    Add benchmarks for array accesses optimizations.
    Change-Id: Ie23ea30ef1f24f266612b628a3cde1985df7d2d0

---
+++ benchmarks/micro/ArrayAccess.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2016 Linaro Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Description:     Tracks performance of accessing array locations using variables and constants
+ *                  as indexes.
+ * Main Focus:      Memory accesses using arrays.
+ *
+ */
+
+package benchmarks.micro;
+
+public class ArrayAccess {
+
+  private static final int ITER_COUNT = 1000;
+
+  public static void AccessArrayConstants(int[] array) {
+    for (int j = 0; j < 100000; j++) {
+      array[4]++;
+      array[5]++;
+    }
+  }
+
+  public static void AccessArrayVariables(int[] array, int i) {
+    for (int j = 0; j < 100000; j++) {
+      array[i]++;
+      array[i + 1]++;
+      array[i + 2]++;
+      array[i - 2]++;
+      array[i - 1]++;
+    }
+  }
+
+  public void timeAccessArrayConstants(int iters) {
+    int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int i = 0; i < iters; i++) {
+      AccessArrayConstants(array);
+    }
+  }
+
+  public void timeAccessArrayVariables(int iters) {
+    int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int i = 0; i < iters; i++) {
+      AccessArrayVariables(array, 5);
+    }
+  }
+
+  public static void main(String[] args) {
+    int rc = 0;
+    ArrayAccess obj = new ArrayAccess();
+
+    long before = System.currentTimeMillis();
+    obj.timeAccessArrayConstants(ITER_COUNT);
+    long after = System.currentTimeMillis();
+    System.out.println("benchmarks/micro/ArrayAccessConstants: " + (after - before));
+
+    before = System.currentTimeMillis();
+    obj.timeAccessArrayVariables(ITER_COUNT);
+    after = System.currentTimeMillis();
+    System.out.println("benchmarks/micro/ArrayAccessVariables: " + (after - before));
+
+    System.exit(rc);
+  }
+}
